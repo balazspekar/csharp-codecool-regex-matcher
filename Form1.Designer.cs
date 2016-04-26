bd4fb4c9220d182c37296aa245c8cea796d59546
Balazs Pekar <peba0706@gmail.com>
2016-04-26 11:21:43 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Matcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPattern = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Text";
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(13, 30);
+            this.txtText.Multiline = true;
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(322, 134);
+            this.txtText.TabIndex = 1;
+            this.txtText.Text = "Edit the Expression & Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(346, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Matched";
+            // 
+            // lstBox
+            // 
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.Location = new System.Drawing.Point(349, 30);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(243, 134);
+            this.lstBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Pattern";
+            // 
+            // txtPattern
+            // 
+            this.txtPattern.Location = new System.Drawing.Point(13, 200);
+            this.txtPattern.Name = "txtPattern";
+            this.txtPattern.Size = new System.Drawing.Size(322, 20);
+            this.txtPattern.TabIndex = 5;
+            this.txtPattern.Text = "/([A-W])\\w+/g";
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(349, 199);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(243, 23);
+            this.btnFind.TabIndex = 6;
+            this.btnFind.Text = "Find Pattern in Text";
+            this.btnFind.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 235);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.txtPattern);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lstBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPattern;
+        private System.Windows.Forms.Button btnFind;
+    }
+}
+
